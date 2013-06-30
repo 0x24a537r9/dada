f99e8a3b35c5256199e46ab1a26d6e3e0f9ce433
0x24a537r9 <0x24a537r9@gmail.com>
2013-06-29 18:59:09 -0700

    Added an Entry model.

--- dada/app/admin.py
+++ dada/app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from models import *
 
-#admin.site.register(Model)
+admin.site.register(Entry)
--- dada/app/models.py
+++ dada/app/models.py
@@ -1,3 +1,6 @@
+import random
+
+from datetime import datetime
 from django.db import models
 
 
@@ -13,4 +16,31 @@ def ellipsis(width):
   return _decorator
 
 
-# Add models here.
+def randint64():
+  return random.getrandbits(64) - 2 ** 63
+
+
+class Entry(models.Model):
+  id = models.BigIntegerField(blank=True, default=randint64, primary_key=True)
+  QUESTION, ANSWER = 'Q', 'A'
+  TYPES = {
+      QUESTION: 'Question',
+      ANSWER: 'Answer',
+  }
+  type = models.CharField(choices=TYPES.items(), max_length=1)
+  text = models.CharField(max_length=64)
+  author = models.CharField(max_length=32)
+  upvotes = models.PositiveIntegerField(blank=True, default=1)
+  downvotes = models.PositiveIntegerField(blank=True, default=0)
+  score = models.FloatField(blank=True, default=0.0)
+  is_flagged = models.BooleanField(blank=True, default=False)
+  created = models.DateTimeField(default=datetime.now, editable=False, blank=True)
+
+  @ellipsis(100)
+  def __unicode__(self):
+    return u'%s: %s \u2014 %s on %s' % (Entry.TYPES[self.type], self.text, self.author,
+                                        datetime.date(self.created).strftime('%b %d, \'%y'))
+
+  class Meta:
+    get_latest_by = 'created'
+    ordering = ['id', '-score', 'created']
